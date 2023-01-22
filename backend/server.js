542513324502ff1d9bc956f4a91d5821743c533d
Ian Kirk <iankirk99@gmail.com>
2023-01-22 12:46:23 -0800

    added embedded sign in

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,16 @@ const fetch = require("node-fetch");
 const jwt = require("jsonwebtoken");
 const app = express();
 const port = 5001;
-const clientId = "egE7AmFhWu40nDx3vm7x9HQOgjuUxHhuU8WP8mjAviY";
+// const clientId = "egE7AmFhWu40nDx3vm7x9HQOgjuUxHhuU8WP8mjAviY";
+// const clientSecret = `M_gO6yeMNkB8w9LPxhKqgE83mbRrIVM9V6uXw6GrOFsVFeaL2BELaoumsegVleZAEPheGwKO3TxIAWcCRleX4FyGml_vGgmObtZuoitc-0JOq6T5S15UF-ah7i5PSH7Bpkx8pclenF_vwl1Gxk9MsEpo05cnR3grFobACSQBDDe3LqpNXuzcZuLko91bYuiTUVs8j947kj0rJGbz5W20kk2xxYNmTRj-8bkRzHzGeli882u-20eU1TA8Lh1vg9hGadGznzw2vBW016CGgSRqXYZBG_NaL9dBoCv0zFmX-dqQ8rn7FShgAqEvTPjQ_AtFXpN2yT_Iujs3egoykJ91Tw`;
+const clientId = "w3qi-IP24Vc6mFafN1ZCUuY5VhtjiU5aCu9hrC50Kg8";
 const clientSecret =
-  "lPi7i_NOLBbI9mVvvpoQWv8W_zFp0kAYp-LeG4Vb2OVt6RFtpvjH21lTurZMEjpZqaD4y6YuS_dKsY2UV8wl4Yu5nWFP9M9RfvyGL6F_QoYM1v1OlZxogpL3fOQEhr73qZx5Y4_goldnUbCxKZwxtQhCTx-mKZj766EoUrCXuM1Ck1S7u4CEy_kTBkiSXFhwpADVjQxzKNgfpZGdeXEDBlBsauZ6Fd13PrY02ez9KjS2_tZnCeTld9sM9PdPc-FJCSnQDQLrcEnWK3YAc4UVJjhHtBU4UbkfgqbTHGUtV8z5ypNyg-jEmZsvqbVe2VYjxJkD9_ZkI4V-VL1ZBiS3IQ";
+  "BjxF6O866PvTa5JPW7ATVobPD4WgNqxh56b2Q9wLpp3sBbbMXJf0zt7rMGlgLnRFygAYlXTaxRwXEIE_iXKlh54pfgHOHB18xgahJRawOCNoBcGUXW0ajDTOn2td3DeZ4gZ_fd3oaMt3Hqzo3WblZTjwV4-LQZEYElW97qCinRZpjxCmc16ZiSCy98olDj_w3cMLWs-xe3IBqbVS9rGMD01zc0r1R9jsbtz0pmB43qnwwN8hxaxrXSbVnt7atmQ5Jt56Uqyts9xoZVOaA8AzhmrXBIWS33J2MCC381VcMAsdj5BYnXKR9cFxX--YueMPYZ77CxAzwkIHr3H0im2cww";
 const redirect_uri = "localhost:3001/redirect";
+// const jwtKey =
+// "HUNTzrvQR-ZUqfLPE25ACSoVdT_G2sF_6XGKwscb5uskj8xGDIkeH9ff1PRuWBzMJl1PtewRUoSnWS39AUyT8LQ4BMUdsQpSIRPFrTaAUAlUUVTLR0G2u_Cz8Cg9dcHgDf36A3WqbL24pGPMitfwdtZnRtx-0UZdT5Ha6YcvAuTKHXKPVVzHLfkNrAyC3ktHe8SHuMLHvOyIpEUfzgaBF0ccmF7Qw3FfXjYbL8GcUPQscNBECg_dEcslH-WUMX0yI4J8wkAkYG-kefEaVpFpbN80GRQsTJGl8ZDFQ85lICSzgQgHxm8AfbQlyAbumxbTvIpZPK6WnPd5MGoxflQvaA";
 const jwtKey =
-  "HUNTzrvQR-ZUqfLPE25ACSoVdT_G2sF_6XGKwscb5uskj8xGDIkeH9ff1PRuWBzMJl1PtewRUoSnWS39AUyT8LQ4BMUdsQpSIRPFrTaAUAlUUVTLR0G2u_Cz8Cg9dcHgDf36A3WqbL24pGPMitfwdtZnRtx-0UZdT5Ha6YcvAuTKHXKPVVzHLfkNrAyC3ktHe8SHuMLHvOyIpEUfzgaBF0ccmF7Qw3FfXjYbL8GcUPQscNBECg_dEcslH-WUMX0yI4J8wkAkYG-kefEaVpFpbN80GRQsTJGl8ZDFQ85lICSzgQgHxm8AfbQlyAbumxbTvIpZPK6WnPd5MGoxflQvaA";
+  "9owmlhpNszhiTM8qRfhuYLe4YcuHsygBERpCNSze9qNk3-F7iyXbhflW8SBy-BVfmAXg69CoBh_j8XB6GCJzn6sHKcJdGzhXHnB7rCYYyJ36U_0ap2XxqK44O_Je1Gn1f2FWmHzeZJYLaFGs8E8UeDWTtIUFG5aNcR9CaP4K2Nqcj6HkbIfXpBFabmhZ17diMx63V8-tJmj4lN4Q47V8xl27KLfNzK8IU4zfaW_JEhJUn9SguWKPzIlHB97nzNIuSnR7i4P-3wglQncv0RonWxf9NCqdmK8-mC7AK0RkwuRIqM5IhvJBJk2HPTApXuigd7Yqjwj61uaZoH2dmyyCMg";
 let codeVerifier;
 let accessToken;
 
@@ -44,8 +48,8 @@ app.get("/codeResponse", async (req, res) => {
   console.log(req.query);
   const code = req.query.code;
   console.log(new Date().toISOString());
-  const url = `https://dev.auth.authillo.com/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect_uri}&code_verifier=${codeVerifier}&client_id=${clientId}&client_secret=${clientSecret}&request_type=OIDC`;
-  // const url = `http://localhost:3000/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect_uri}&code_verifier=${codeVerifier}&client_id=${clientId}&client_secret=${clientSecret}&request_type=OIDC`;
+  // const url = `https://dev.auth.authillo.com/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect_uri}&code_verifier=${codeVerifier}&client_id=${clientId}&client_secret=${clientSecret}&request_type=OIDC`;
+  const url = `http://localhost:3000/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect_uri}&code_verifier=${codeVerifier}&client_id=${clientId}&client_secret=${clientSecret}&request_type=OIDC`;
 
   const tokenRes = await fetch(url, {
     method: "POST",
@@ -74,15 +78,16 @@ app.get("/codeResponse", async (req, res) => {
   console.log("userInfo in codeResponse: ", userInfo);
   res.json(
     JSON.stringify({
-      idToken: verifiedToken,
+      idTokenParsed: verifiedToken,
+      idToken,
       userInfo,
     })
   );
 });
 
 const userInfoReq = async (token) => {
-  const url = `https://dev.auth.authillo.com/userinfo`;
-  // const url = `http://localhost:3000/userinfo`;
+  // const url = `https://dev.auth.authillo.com/userinfo`;
+  const url = `http://localhost:3000/userinfo`;
 
   const useRInfoRes = await fetch(url, {
     headers: {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { createBrowserRouter } from "react-router-dom";
 import { Redirect } from "./Redirect";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const router = createBrowserRouter([
   {
@@ -15,11 +15,62 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  const [image, setImage] = useState(null);
+  const [codeChallenge, setCodeChallenge] = useState("");
+  const [userId, setUserId] = useState("");
+  const [jwt, setJwt] = useState("");
+  const [parsedJWT, setParsedJWT] = useState("");
+  const clientId = "";
+  useEffect(() => {
+    console.log("adding listener");
+    window.addEventListener("message", (event) => {
+      console.log("message data: ", event.data);
+      console.log("message: ", event);
+      if (event.origin !== "http://localhost:4200") {
+        console.log("invalid origin");
+        return;
+      }
+      const code = event.data;
+      fetch(
+        "http://localhost:5001/codeResponse?" + new URLSearchParams({ code })
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((response) => {
+          console.log(response, "response from fetch");
+          console.log("userInfo:", response?.userInfo);
+          try {
+            const parse = JSON.parse(response);
+            console.log(parse);
+            console.log(parse?.userInfo);
+            console.log(parse?.idToken);
+            // setName(parse?.userInfo?.userAttributes?.nickname);
+            // setUserInfo(parse?.userInfo);
+            setUserId(parse?.userInfo?.userAttributes?.sub);
+            setJwt(parse?.idToken);
+            setParsedJWT(parse?.idTokenParsed);
+          } catch (err) {
+            console.log("error: ", err);
+          }
+        });
+    });
+  }, []);
+  useEffect(() => {
+    console.log("app mounted");
+    fetch("http://localhost:5001/getCodeChallenge")
+      .then((res) => {
+        return res.json();
+      })
+      .then((response) => {
+        const parsedResponse = JSON.parse(response);
+        console.log(parsedResponse, "response from fetch");
+        setCodeChallenge(parsedResponse?.codeChallenge);
+      });
+  }, []);
   return (
     <div className="App">
       <h1>Welcome to Authillo React integration example</h1>
-      <div>
+      {/* <div>
         <button
           onClick={() => {
             fetch("http://localhost:5001/getCodeChallenge")
@@ -29,7 +80,7 @@ function App() {
               .then((response) => {
                 const parsedResponse = JSON.parse(response);
                 console.log(parsedResponse, "response from fetch");
-                const scopes = `openid name`;
+                const scopes = `openid`;
                 const clientId = "egE7AmFhWu40nDx3vm7x9HQOgjuUxHhuU8WP8mjAviY";
                 const maxAge = 3600;
                 const codeChallenge = parsedResponse.codeChallenge;
@@ -52,7 +103,7 @@ function App() {
               .then((response) => {
                 const parsedResponse = JSON.parse(response);
                 console.log(parsedResponse, "response from fetch");
-                const scopes = `openid name face`;
+                const scopes = `openid face`;
                 const clientId = "egE7AmFhWu40nDx3vm7x9HQOgjuUxHhuU8WP8mjAviY";
                 const maxAge = 3600;
                 const codeChallenge = parsedResponse.codeChallenge;
@@ -65,6 +116,30 @@ function App() {
           Login (face required)
         </button>
       </div>
+      <div style={{ margin: "3rem" }}>
+        <iframe
+          src={`http://localhost:4200/iframe/popup?platform_name=Test%20Name&response_type=code&scope=openid&state=undefined&redirect_uri=localhost:3001/redirect&client_id=TestIntegration&max_age=3600&code_challenge=notreal&code_challenge_method=S256`}
+          height="200"
+          width="350"
+          style={{ border: "none", borderRadius: ".5rem" }}
+        >
+          Oops
+        </iframe>
+      </div> */}
+      <div style={{ margin: "3rem" }}>
+        {codeChallenge != "" && (
+          <iframe
+            src={`http://localhost:4200/iframe/embedsignin?platform_name=Test%20Name&response_type=code&scope=openid&state=undefined&redirect_uri=localhost:3001/redirect&client_id=w3qi-IP24Vc6mFafN1ZCUuY5VhtjiU5aCu9hrC50Kg8&max_age=3600&code_challenge=${codeChallenge}&code_challenge_method=S256`}
+            height="50"
+            width="350"
+            style={{ border: "none", borderRadius: "25px" }}
+          >
+            Oops
+          </iframe>
+        )}
+      </div>
+      {userId !== "" && userId !== undefined && <h2>Hello {userId}</h2>}
+      {<p>JWT: {JSON.stringify(jwt)}</p>}
     </div>
   );
 }
